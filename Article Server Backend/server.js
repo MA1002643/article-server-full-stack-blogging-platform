4bc198f9dc3f927f7909ab7e9924928f758b1ca2
MA1002643 <87866666+MA1002643@users.noreply.github.com>
2025-10-10 06:28:07 +0100

    Fixed the port connection between the frontend and the backend.

--- Article Server Backend/server.js
+++ Article Server Backend/server.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
 // Server port
 const HTTP_PORT = 3333;
-
-// Start server
-app.listen(HTTP_PORT, () => {
-  console.log("Server running on port: " + HTTP_PORT);
-});
 
 // Logging
 app.use(morgan("tiny"));
@@ -17,6 +13,15 @@ app.use(morgan("tiny"));
 // Body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Enable CORS for all routes
+// Enable CORS for all routes and allow the X-Authorization header used by the frontend
+app.use(
+  cors({
+    origin: true,
+    allowedHeaders: ["Content-Type", "X-Authorization"],
+  })
+);
 
 // Root endpoint
 app.get("/", (req, res, next) => {
@@ -30,4 +35,9 @@ require("./app/routes/comments.routes")(app);
 // Default response for any other request
 app.use(function (req, res) {
   res.sendStatus(404);
+});
+
+// Start server (after middleware and routes are registered)
+app.listen(HTTP_PORT, () => {
+  console.log("Server running on port: " + HTTP_PORT);
 });
